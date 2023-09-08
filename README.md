b1a63e87c98e810efd30939d20e1a72a01a5078f
Agostinho Felipe <agostinhofelipeneto@outlook.com>
2023-09-07 22:20:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Beatemup_Project
+
+Developed with Unreal Engine 5
---
+++ Source/Beatemup_Project/BeatemupGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeatemupGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATEMUP_PROJECT_API ABeatemupGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
